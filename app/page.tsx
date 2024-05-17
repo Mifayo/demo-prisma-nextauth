fa19c255f3ade383605968bbcda444fd3cfc6abf
Faysal MISSI <faysal.missi@gmail.com>
2024-05-17 21:35:40 +0100

    page home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
-import Image from "next/image";
+import { LoginButton } from "@/components/auth/login-button";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <>Hello this is my App</>
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <div className="space-y-6 text-center">
+        <h1 className="text-6xl font-semibold text-white drop-shadow-md">
+          🔐 Auth
+        </h1>
+        <p className="text-white text-lg">A simple authentication service</p>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Login
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   );
 }
