fb39392717712fa965d17c1d85f2036738f48f8b
Faysal MISSI <faysal.missi@gmail.com>
2024-05-23 17:16:00 +0100

    login func

--- actions/login.ts
+++ actions/login.ts
@@ -3,13 +3,11 @@ import { LoginSchema } from "@/schemas";
 import * as z from "zod";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
-  const res = await fetch(`${process.env.API_HOST}/api/login`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(values)
-  });
-  const data = await res.json();
-  return data;
+
+  const validatedFields = LoginSchema.safeParse(values);
+
+  if (!validatedFields.success)
+    return {error: "Invalid inputs"};
+
+  return {success: "Email sent"};
 };
